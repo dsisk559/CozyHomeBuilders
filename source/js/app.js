4a58a643c5bf26687183f6370fd7226b8e046180
Daniel Sisk <dsisk559@gmail.com>
2018-08-12 14:33:17 -0700

    Completed Navigation

--- source/js/app.js
+++ source/js/app.js
@@ -1,10 +1,20 @@
+var navButton = document.getElementById("nav-toggle-button");
 
+function toggleNavDisplay(event) {
+  event.preventDefault();
+
+  var mainNav = document.getElementById("mainNavigation");
+
+  mainNav.classList.toggle("showNav");
+}
+
+navButton.addEventListener("click", toggleNavDisplay);
 // JavaScript
-window.sr = ScrollReveal(); 
+window.sr = ScrollReveal();
 
 // sr.reveal('h1', {
 //     delay: 0,
 //     duration: 200,
 //     origin: 'bottom',
-//     distance: '100px' 
+//     distance: '100px'
 // });
